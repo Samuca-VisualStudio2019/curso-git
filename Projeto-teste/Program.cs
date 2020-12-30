99476a475f75f64b9c8b3bd1ba0882ff3354ca70
Samuel Barreto <samucateclas@gmail.com>
2020-12-30 16:50:23 -0300

    Acrescentado a frase como voce está

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -7,7 +7,8 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como voce está?");
 
             Console.ReadLine();
         }
